07ffa1d0ad92269105547c39a49434481fc90e4e
Dyego Alves <dyego.pereira@ufv.br>
2021-12-10 23:32:00 -0300

    fix_tabbles.py archive created to fix tabbles bugs using pandas library tabble 31 bugs initialized to be fixed

---
+++ fix_tabbles.py
@@ -0,0 +1,56 @@
+import pandas as pd
+
+
+def fix_tabble_31(tabble31_parts):
+    
+    # fixing first part of tabble 31:
+    # Renaming the collumns indexies
+    part1 = tabble31_parts[1]
+    part1.rename(columns={'Unnamed: 0':"Código",
+    'Tabela de Categoria do Padrão TISS':'Descrição da categoria'
+    },inplace = True
+    )
+    part1.loc[25,'Código'] = ''
+    part1.loc[25,'Descrição da categoria'] = ''
+    
+    for i in range (0,25):
+        novo_codigo = part1['Código'][i+1]
+        nova_descricao = part1['Descrição da categoria'][i+1]
+        part1.loc[i,'Código'] = novo_codigo
+        part1.loc[i,'Descrição da categoria'] = nova_descricao
+    part1 = part1.drop([24,25])
+    print(part1)
+    print('###################')
+
+    # fix second part of tabble 31
+    part2 = tabble31_parts[2]
+    print(part2)
+    print("####################")
+    part2.loc[26,'38'] = ''
+    part2.loc[26,'Terminologia de mensagens (glosas, negativas e outras)'] = ''
+    range_list = list(reversed(range(1,27)))
+    print(range_list)
+    for j in range_list:
+        novo_codigo = int(part2['38'][j-1])
+        nova_descricao = part2['Terminologia de mensagens (glosas, negativas e outras)'][j-1]
+        part2.loc[j,'38'] = novo_codigo
+        part2.loc[j,'Terminologia de mensagens (glosas, negativas e outras)'] = nova_descricao
+    print(part2)
+    part2.loc[0,'38'] = 38
+    part2.loc[0,'Terminologia de mensagens (glosas, negativas e outras)'] = 'Terminologia de mensagens (glosas, negativas e outras)'
+
+    part2.rename(columns={'38':"Código",
+    'Terminologia de mensagens (glosas, negativas e outras)':'Descrição da categoria'
+    },inplace = True
+    )
+    print(part2)
+    df = pd.concat([part1,part2],ignore_index=True)
+    print(df)
+    return df
+
+def fix_tabble30(tabble30):
+    return tabble30
+
+def fix_tabble32(tabble32):
+    return tabble32
+
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from pandas.core.reshape.concat import concat
 import tabula
 import pandas as pd
 from zipfile import ZipFile
-
+from fix_tabbles import *
 
 pdf_directory = "Componente_organizacional.pdf"
 
@@ -14,15 +15,6 @@ def extract_tabbles(pdf_file):
         print('Não foi possível extrair tabelas do arquivo pdf')
         return []
 
-def fix_tabble_31(tabble31_parts):
-    pass
-
-def fix_tabble30(tabble30):
-    return tabble30
-
-def fix_tabble32(tabble32):
-    return tabble32
-
 def create_csv(file_name,file_content):
     try:
         with open(f'{file_name}.csv','w') as file:
@@ -31,6 +23,7 @@ def create_csv(file_name,file_content):
     except:
         return False
 
+#Compacta os arquivos cvs à um arquivo .zip 
 def zip_files(files_list):
     with ZipFile('tabbles.zip','w') as zip:
         zip.write(files_list[0])
@@ -41,30 +34,19 @@ def main():
     tabble_list = extract_tabbles(pdf_directory)
     quadro_30 = tabble_list[0]
     quadro_32 = tabble_list[7]
-    quadro_31 = pd.concat([tabble_list[1],
-    tabble_list[2],
-    tabble_list[3],
-    tabble_list[4],
-    tabble_list[5],
-    tabble_list[6]]
-    )
+
     csv_quadro30 = quadro_30.to_csv()
     csv_quadro32 = quadro_32.to_csv()
     create_csv('quadro30',csv_quadro30)
     create_csv('quadro32',csv_quadro32)
-    #print(dataframe.columns.tolist())
-
-    for i in range (1,7):
-        print(tabble_list[i])
-        print('#############################')
 
     # Chama as funções que consertam as tabelas 
     fixed_tabble30 = fix_tabble30(tabble_list[0])
     fixed_tabble31 = fix_tabble_31(tabble_list)
     fixed_tabble32 = fix_tabble32(tabble_list[7])
     csv_quadro30 = fixed_tabble30.to_csv()
-    csv_quadro31 = fixed_tabble30.to_csv()
-    csv_quadro30 = fixed_tabble30.to_csv()
+    csv_quadro31 = fixed_tabble31.to_csv()
+    csv_quadro30 = fixed_tabble32.to_csv()
     
     # Cria csv das tabelas consertadas
     create_csv('quadro30',csv_quadro30)
@@ -76,9 +58,6 @@ def main():
     zip_files(files_dir)
 
 main()
-
-
-    
 
 
 #Tarefas:
